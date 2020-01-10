0c80180ccc8d70056af32541bf07c9b7e446972d
Emil Sroka <srokaemil@gmail.com>
2020-01-10 18:11:48 +0100

    Refactor exercises 2-3 in lab4_2
    Remove lab4_0.pl

--- lab4_2.pl
+++ lab4_2.pl
@@ -2,33 +2,18 @@
 % Proszę napisać predykat is_empty_diff/1, który sprawdzi czy dana lista różnicowa jest pusta.
 is_empty_diff(List-T) :- List == T. 
 
+%%% ZADANIE 2 %%%
+% Proszę napisać predykat length_diff/2, który policzy długość listy różnicowej.
+length_diff(List, 0) :- is_empty_diff(List).
+length_diff([Head|SubList]-T, Result) :- 
+    \+ is_empty_diff([Head|SubList]-T), 
+    length_diff(SubList-T, SubResult), 
+    Result is SubResult + 1.  
 
-
-
-% TO DO 2, 3
-
-% Proszę napisać predykat length_diff/2, który policzy długość listy różnicowej:
-/*
-length_diff(List-T, 0) :- is_empty_diff(List-T).
-length_diff([_|List]-T, Result) :- 
-    length_diff(List-T, SubResult), 
-    Result is SubResult + 1.
-*/
-
-length_diff(List-E, 0) :- is_empty_diff(List-E).
-length_diff(List-E, Result) :- 
-    \+ is_empty_diff(List-E),
-    List = [_|SubList],
-    length_diff(SubList-E,SubResult), 
-    Result is SubResult + 1.
-
+%%% ZADANIE 3 %%%
 % Proszę napisać predykat member_diff/2, który sprawdzi czy zadany element należy do listy różnicowej.
-/*
-member_diff(Element, List-T) :- \+ is_empty_diff(List-T), [H|_] = List, H = Element.
-member_diff(Element, List-T) :- \+ is_empty_diff(List-T), [_|SubList] = List, member_diff(Element, SubList-T).
-*/
-member_diff(Element, [Element|_]-_).
-member_diff(Element, [H|List]-T) :-
-    H \= Element,
-    \+ is_empty_diff(List-T),  
-    member_diff(Element, List-T).
+member_diff(Element, [Element|_]-_). 
+member_diff(Element, [Head|SubList]-T) :- 
+    Element \= Head, 
+    \+ is_empty_diff(SubList-T), 
+    member_diff(Element, SubList-T).
